0df054903f6214ae7cba957114a42ee1c1acb0eb
girish-nvmg <girish.nvmg@gmail.com>
2019-05-07 14:37:44 +0200

    clean

--- java1/src/main/java/com/company/customerprofile/dao/CustomerProfileDAO.java
+++
@@ -1,9 +0,0 @@
-package com.company.customerprofile.dao;
-
-import java.util.List;
-import com.company.customerprofile.entities.CustomerProfile;
-
-public interface CustomerProfileDAO {
-	public List<CustomerProfile> getCustomerProfiles();
-
-}
