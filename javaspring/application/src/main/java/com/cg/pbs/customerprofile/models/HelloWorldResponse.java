5141fa508b9fb84de35f0bdcff09cf605a4c4d8b
girish-nvmg <girish.nvmg@gmail.com>
2019-05-07 16:24:21 +0200

    update

---
+++ javaspring/application/src/main/java/com/cg/pbs/customerprofile/models/HelloWorldResponse.java
@@ -0,0 +1,11 @@
+package com.cg.pbs.customerprofile.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class HelloWorldResponse
+{
+    @JsonProperty("hello")
+    private final String name;
+}
